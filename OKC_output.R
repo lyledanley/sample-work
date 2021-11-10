a7d05ff35828e8ad4beb59351eb3bf0b80f4955f
lyledanley <70077937+lyledanley@users.noreply.github.com>
2021-11-10 14:07:01 -0600

    Add files via upload

---
+++ OKC_output.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(readr)
+library(formattable)
+
+shots <- read_csv("OKC/Datasets/shots_data.csv")
+
+#deriving shot_type based on dimensions given and plotting points on a circle
+shots <- shots |>
+  mutate(shot_type = 
+           ifelse(abs(x) < 22 & y < 7.8, "2PT", 
+                  ifelse(abs(x) > 22 & y <= 7.8, "C3",
+                         ifelse(x < 22 & y > 7.8 & sqrt(x^2+y^2) >= 23.75, 
+                                "NC3", "2PT")
+                         )
+                  )
+         )
+#deriving shot percentages based on made/attempted for each shot type
+
+shots |>
+  group_by(team, shot_type) |>
+  summarise(attempted = n()) |>
+  ungroup()|>
+  group_by(team) |>
+  mutate(distribution = round(attempted/sum(attempted),3)) |>
+  select(team, shot_type, distribution) -> shot_distribution
+
+
+#deriving eFG based on formula given. Since 2PT shots have no weight to the (0.5*3PM), using a conditional formula based on shot type.
+eFG_distribution <- shots |>
+  group_by(team, shot_type, fgmade) |>
+  summarise(made = n()) |>
+  mutate(total = sum(made)) |>
+  filter(fgmade == 1) |> 
+  mutate(eFG = case_when(shot_type == "2PT" ~ made/total, 
+                      TRUE ~ (made + 0.5*made)/total)) |>
+  mutate(eFG = round(eFG, 3)) |>
+  select(team, shot_type, eFG)
+
+#joining the two derived columns together for one output df
+answer_output <- shot_distribution |> left_join(eFG_distribution, by = c("team", "shot_type"))
+
+formattable(answer_output)
